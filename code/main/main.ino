1ee37d88c53905cdc28596fcba93fe19e80772d0
= <gironclement@orange.fr>
2018-09-24 22:12:10 +0200

    set up second wheel

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,81 +1,115 @@
-int PulseA = 4;   // Borne Step du Driver
-int DirA = 3;     // Borne DirA du Driver
-int Slp = 2;     // Borne Sleep du Driver
+/* Engine A */
+// pins
+int PulseA = 4;                         // Borne Pulse
+int DirA = 3;                           // Borne Dir
+int SlpA = 2;                           // Borne Sleep
+// Variables
+int sensA = 1;                          // Direction
+unsigned long previousLeftStep = 0;     // Time of last step
+bool previousLeftState = false;         // State of last step
 
-int val_sw = 0;
+/* Engine B */
+// pins
+int PulseB = 7;                         // Borne Pulse
+int DirB = 6;                           // Borne Dir
+int SlpB = 5;                           // Borne Sleep du Driver
+// variables
+int sensB = -1;                         // Direction
+unsigned long previousRightStep = 0;    // Time of last step
+bool previousRightState = false;        // State of last step
 
+/* Joystick */
+// pins
 int SW_pin = A2;  // Switch
 int X_pin = A0;  // Borne VAR X du Switch
 int Y_pin = A1;  // Borne VAR X du Switch
+
+// variables
+int val_sw = 0;
 int val_var_X = 0;
 int val_var_Y = 0;
 int init_var_X = 0;
 int init_var_Y = 0;
-int delaiA = 0;
-int freqY = 0;
-int minDelay = 200;
-int maxDelay = 2000;
 
-
+/* Led */
+// pin
 int ledPin = 13;  // LED Integree
 
-unsigned long previousLeftStep = 0;
-bool previousLeftState = false;
+/* Global variables */
+int freqY = 0;        // frequency between to steps of the engines, to be computed
+int minDelay = 200;   // minimum delay between engine steps
+int maxDelay = 2000;  // maximum delay between engine steps (above=no speed)
 
 void setup()
 {
+  // setup joysticks pins
   pinMode(X_pin, INPUT);
   pinMode(Y_pin, INPUT);
   pinMode(SW_pin, INPUT);
 
+  // setup engine pins
   pinMode(PulseA,OUTPUT);
   pinMode(DirA,OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(Slp, OUTPUT);
 
-  //Serial.begin (115200);
-
+  // get initial joystick values
   init_var_X = analogRead(X_pin);
   init_var_Y = analogRead(Y_pin);
+
+  //Serial.begin (115200);
 }
 
-void ComputeWheelStep(){
+
+void ComputeWheelStep(unsigned long previousStep, bool previousState, int dir, int sens, int pulse){
   // read joystick values
   val_var_X = analogRead(X_pin);
   val_var_Y = analogRead(Y_pin);
   val_sw = analogRead(SW_pin);
 
-  // compute delay
+  // compute frequency
   freqY = val_var_Y - init_var_Y;
-  delaiA = 5000 - 10*abs(freqY);
-  if (delaiA <= maxDelay){
-    if (delaiA <= minDelay){
-      delaiA = minDelay;
-    }
 
-    unsigned long delaiAus = 0;
-    delaiAus = (unsigned long) delaiA;
+  // infer engines steps delay from frequency
+  int delai;
+  delai = 5000 - 10*abs(freqY);
+
+  // if delay small enough, move
+  if (delai <= maxDelay){
+    // if delay too small, threshold it
+    if (delai <= minDelay){
+      delai = minDelay;
+    }
+    // convert int delay into unsigned long type (to handle big numbers)
+    unsigned long delaius;
+    delaius = (unsigned long) delaiA;
     unsigned long now = micros();
-    if (now - previousLeftStep >= delaiAus){
+    // if last step is far enough, make new step
+    if (now - previousStep >= delaius){
       digitalWrite(Slp,HIGH);
-      if (freqY >= 0){
-        digitalWrite(DirA, HIGH);
+      // choose the direction of the step
+      if (sens*freqY >= 0){
+        digitalWrite(dir, HIGH);
       }
       else {
-        digitalWrite(DirA,LOW);
+        digitalWrite(dir,LOW);
       }
-
-      previousLeftState = !previousLeftState; // etait = ~ previousLeftState;
-      digitalWrite(PulseA, previousLeftState);
-
-      previousLeftStep = now;
+      // execute the step
+      previousState = !previousState; // etait = ~ previousLeftState;
+      digitalWrite(pulse, previousState);
+      // update last step time
+      // THIS MAY CAUSE A FAIL, IDK IF THE previousStep variable is updated globally
+      previousStep = now;
     }
   }
 }
 
 void loop()
 {
-  ComputeWheelStep();
+  // Compute step for left wheel
+  ComputeWheelStep(previousLeftStep, previousLeftState, DirA, sensA, PulseA);
+  // Compute step for right wheel
+  ComputeWheelStep(previousRightStep, previousRightState, DirB, sensB, PulseB);
   delayMicroseconds(20);
   //Serial.println(freqY);
 
